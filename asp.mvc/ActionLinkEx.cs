3387c8ea6aea68cc876df05a226315d2382f8c68
serkulaga <serkulaga@gmail.com>
2014-09-10 20:42:56 +0300

    Update ActionLinkEx.cs

--- asp.mvc/ActionLinkEx.cs
+++ asp.mvc/ActionLinkEx.cs
@@ -1,44 +1,44 @@
-		/// <summary>
-		/// This function adds supporting of array params for RouteValueDictionary
-		/// </summary>
-		/// <returns></returns>
-		public static MvcHtmlString ActionLinkEx(this HtmlHelper helper, string text, string action, string controller, object routeData, object htmlAttributes)
-		{
-			var urlHelper = new UrlHelper(helper.ViewContext.RequestContext);
-			string href = urlHelper.Action(action, controller);
+/// <summary>
+/// This function adds supporting of array params for RouteValueDictionary
+/// </summary>
+/// <returns></returns>
+public static MvcHtmlString ActionLinkEx(this HtmlHelper helper, string text, string action, string controller, object routeData, object htmlAttributes)
+{
+	var urlHelper = new UrlHelper(helper.ViewContext.RequestContext);
+	string href = urlHelper.Action(action, controller);
 
-			if (routeData != null)
+	if (routeData != null)
+	{
+		var routeValues = routeData is RouteValueDictionary ? (RouteValueDictionary)routeData : new RouteValueDictionary(routeData);
+		var urlParameters = new List<string>();
+		foreach (var key in routeValues.Keys)
+		{
+			var value = routeValues[key];
+			if (value != null)
 			{
-				var routeValues = routeData is RouteValueDictionary ? (RouteValueDictionary)routeData : new RouteValueDictionary(routeData);
-				var urlParameters = new List<string>();
-				foreach (var key in routeValues.Keys)
+				if (value is IEnumerable && value.GetType() != typeof(string))
 				{
-					var value = routeValues[key];
-					if (value != null)
+					foreach (var val in (IEnumerable)value)
 					{
-						if (value is IEnumerable && value.GetType() != typeof(string))
-						{
-							foreach (var val in (IEnumerable)value)
-							{
-								urlParameters.Add(String.Format("{0}={1}", key, val));
-							}
-						}
-						else
-						{
-							urlParameters.Add(String.Format("{0}={1}", key, value));
-						}
+						urlParameters.Add(String.Format("{0}={1}", key, val));
 					}
 				}
-				var paramString = String.Join("&", urlParameters);
-				if (!String.IsNullOrEmpty(paramString))
+				else
 				{
-					href += "?" + paramString;
+					urlParameters.Add(String.Format("{0}={1}", key, value));
 				}
 			}
-
-			var builder = new TagBuilder("a");
-			builder.Attributes.Add("href", href);
-			builder.MergeAttributes(new RouteValueDictionary(htmlAttributes));
-			builder.SetInnerText(text);
-			return new MvcHtmlString(builder.ToString(TagRenderMode.Normal));
 		}
+		var paramString = String.Join("&", urlParameters);
+		if (!String.IsNullOrEmpty(paramString))
+		{
+			href += "?" + paramString;
+		}
+	}
+
+	var builder = new TagBuilder("a");
+	builder.Attributes.Add("href", href);
+	builder.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+	builder.SetInnerText(text);
+	return new MvcHtmlString(builder.ToString(TagRenderMode.Normal));
+}
